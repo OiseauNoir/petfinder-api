a5483085d790099b90abc6d8c67291c550e3febd
Shirish Dali <designsbydali@gmail.com>
2016-08-26 15:51:41 -0500

    added a model folder and created a model-pet.js

---
+++ models/model-pet.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var petsSchema = {
+  task: String,
+  completed: {type:Boolean, default: false},
+  userId: String
+}
+
+const Pets = mongoose.model('Pets', petsSchema);
+
+
+module.exports = model-pet;
