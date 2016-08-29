27190508d0e3d5ee9cd33cf9593c04a134f26bc2
Shirish Dali <designsbydali@gmail.com>
2016-08-29 13:36:08 -0500

    changed the filename to dog

--- routes/pets.js
+++ routes/pets.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var Pet = require('../models/dog');
 
 // GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'OLA' });
 });
+
+
 
 module.exports = router;
