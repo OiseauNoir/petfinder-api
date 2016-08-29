65a5cdabd676b58f0d0bec293da6b0c2dd22aa5f
Shirish Dali <designsbydali@gmail.com>
2016-08-29 16:08:24 -0500

    the dog router is working

--- routes/dogs.js
+++ routes/dogs.js
@@ -3,10 +3,13 @@ var router = express.Router();
 var Dog = require('../models/dog');
 
 // GET dogs list.
-router.get('/', function(req, res, next) {
-  res.json('dogs');
+router.get('/', function (req, res, next) {
+  Dog.find({}, function (err, dogs) {
+    if (err) {
+      res.status(500).send()
+    } else {
+      res.json(dogs)
+    }
+  })
 });
-
-
-
 module.exports = router;
