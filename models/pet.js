fd78eee9fb24a5cec94200a9d54c6f2f5ac868e9
Shirish Dali <designsbydali@gmail.com>
2016-08-29 12:15:32 -0500

    created a petsSchema under the models folder with pet.js

--- models/pet.js
+++ models/pet.js
@@ -1,12 +1,17 @@
-// const mongoose = require('mongoose');
-//
-// var petsSchema = {
-//   task: String,
-//   completed: {type:Boolean, default: false},
-//   userId: String
-// }
-//
-// const Pets = mongoose.model('Pets', petsSchema);
-//
-//
-// module.exports = model-pet;
+const mongoose = require('mongoose');
+
+var petsSchema = {
+  breed: String,
+  kid-friendly: Boolean,
+  maintenance: String,
+  noise-level: String,
+  fullgrownsize: String,
+  personality: String
+}
+
+const Pets = mongoose.model('Pets', petsSchema);
+
+
+module.exports = Pets;
+
+// {type:Boolean, default: false}
