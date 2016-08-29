eddbb621862ebb4488f473d493915a1da5c8c2d1
Shirish Dali <designsbydali@gmail.com>
2016-08-29 14:25:41 -0500

    updated dogs.js under routes, and changed the pet.js under models to dogs.js

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var Pet = require('../models/dog');
+var Dog = require('../models/dog');
 
 // GET home page.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OLA' });
+  res.json('dogs');
 });
 
 
