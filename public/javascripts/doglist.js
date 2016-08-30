1f53914cb42d66cc2027dfae6a31ac256c664e47
Shirish Dali <designsbydali@gmail.com>
2016-08-30 12:01:07 -0500

    created doglist.js

---
+++ public/javascripts/doglist.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  loadDogs();
+});
+
+//load all available dogs for adoption
+function loadDogs() {
+  $.ajax({
+      url:'http://localhost:3000/api/dogs',
+      method: 'GET'
+    })
+    .done(function(dogList) {
+      // console.log(data);
+      for (var i = 0; i <dogList.length; i++) {
+        loadDog(dogList[i])
+      }
+    })
+}
+
+//LOAD  DOG inside this ul
+function loadDog(dog) {
+  console.log(dog);
+  var li = $('<li></li>')
+  li.text(dog.breed)
+  $('.dogs').prepend(li)
+}
