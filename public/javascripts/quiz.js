b71fafd8ee8a687d28d5f734d9ccd38c3f937051
jesse houghton <OiseauNoir>
2016-08-30 16:01:08 -0500

    matched questions with attributes

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -3,3 +3,32 @@ $(document).ready(function() {
 
   });
 });
+
+
+$('#a1') = kidFriendly: true
+$('#a2') = kidFriendly: true
+$('#a3') = kidFriendly: false
+
+$('#b1') = maintenance: 'High'
+$('#b2') = maintenance: 'Medium'
+$('#b3') = maintenance: 'Low'
+
+$('#c1') = noiseLevel: 'High'
+$('#c2') = noiseLevel: 'Medium'
+$('#c3') = noiseLevel: 'Low'
+
+$('#d1') = fullGrownSize: 'Small'
+$('#d2') = fullGrownSize: 'Medium'
+$('#d3') = fullGrownSize: 'Average'
+
+$('#e1') = personality: 'Athlete'
+$('#e2') = personality: 'Intellectual'
+$('#e3') = personality: 'Lone Wolf'
+$('#e4') = personality: 'Defender'
+$('#e5') = personality: 'Entertainer'
+$('#e6') = personality: 'Cuddler'
+
+
+//1. filter quiz entry to return abbreviated list
+
+//2. get that list from the mongo db
