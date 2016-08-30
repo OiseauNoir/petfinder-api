31bfeb3c654929cb9af66d1a1de82d4a0f914ab2
Shirish Dali <designsbydali@gmail.com>
2016-08-29 21:13:11 -0500

    logic to loading dogs on see all dogs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
-  getDogs();
+  // loadDogs();
 
-  $('#all-btn').on('click', function (e) {
+  $('#dogs').on('click', function (e) {
     e.preventDefault();
-    getDogs();
+    // console.log('hello');
+    loadDogs();
   })
+  loadDog()
 
 });
 
-function getDogs() {
-  var api = '';
+
+function loadDogs() {
   $.ajax({
-    url: api
-  }).done(function (data) {
-    console.log(data);
-    // $('#petsList').text(data.value.name)
-  })
+      url:'http://localhost:3000/api/dogs',
+      method: 'GET'
+    })
+    .done(function(dogList) {
+      // console.log(data);
+      for (var i = 0; i <dogList.length; i++) {
+        loadDog(dogList[i])
+      }
+    })
+}
+
+//LOAD  BANDS
+function loadDog(dog) {
+  var li = $('<li></li>')
+  li.text(dog.breed)
+  $('.dogs').prepend(li)
 }
