fe945e29c0ce5805485eabc2a12148ac69c44a0a
Shirish Dali <designsbydali@gmail.com>
2016-08-31 12:34:16 -0500

    nothing to update

--- routes/dogs.js
+++ routes/dogs.js
@@ -4,6 +4,7 @@ var Dog = require('../models/dog');
 
 // GET dogs list.
 router.get('/', function (req, res, next) {
+  console.log(req.query);
   Dog.find(req.query, function (err, dogs) {
     if (err) {
       res.status(500).send()
