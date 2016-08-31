5e086e6f5c5617b402328d1800ff29af118c8838
Shirish Dali <designsbydali@gmail.com>
2016-08-31 17:18:12 -0500

    css for doglist

--- public/javascripts/doglist.js
+++ public/javascripts/doglist.js
@@ -26,14 +26,21 @@ function loadDog(dog) {
   var img = $("<img />").addClass('dogPics')
   img.attr('src', '/img/dog_pics/' + dog.imageName);
   li.append(img)
-  li.append(dogFeature('Breed: ' +dog.breed, 'breed'))
+
   var kidFriendlyText = dog.kidFriendly ? 'Kid Friendly' : 'NOT KID FRIENDLY'
 
-  li.append(dogFeature(kidFriendlyText, 'kidFriendly'))
-  li.append(dogFeature('Maintenance: ' + dog.maintenance, 'maintenance'))
-  li.append(dogFeature('Noise Level: ' + dog.noiseLevel, 'noiseLevel'))
-  li.append(dogFeature('Size: ' + dog.fullGrownSize, 'fullGrownSize'))
-  li.append(dogFeature('Personality: ' + dog.personality, 'personality'))
+  var div = $('<div />')
+  div.addClass('features')
+
+
+  div.append(dogFeature('Breed: ' +dog.breed, 'breed'))
+  div.append(dogFeature(kidFriendlyText, 'kidFriendly'))
+  div.append(dogFeature('Maintenance: ' + dog.maintenance, 'maintenance'))
+  div.append(dogFeature('Noise Level: ' + dog.noiseLevel, 'noiseLevel'))
+  div.append(dogFeature('Size: ' + dog.fullGrownSize, 'fullGrownSize'))
+  div.append(dogFeature('Personality: ' + dog.personality, 'personality'))
+
+  li.append(div)
 
   $('.dogs').prepend(li)
 }
